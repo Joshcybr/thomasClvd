d1417eeb0d01dec47a705f285b8cc843c53f5f71
Administrator <Administrator@DESKTOP-NEII4G1>
2024-04-02 12:22:54 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace thomasClvd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Models/Table_1_clvd.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+namespace thomasClvd.Models
+{
+    public class Table_1_clvd : Controller
+    {
+        public static string con_string = "Server=tcp:thomasclvd.database.windows.net,1433;Initial Catalog=thomasClvd;Persist Security Info=False;User ID=JoshuaThomas;Password=0825521641Jt#;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30";
+        public static SqlConnection con = new SqlConnection (con_string);
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
